334e28ab000d7fb5359693f1d7b913c92fb46c75
Seiji Akakabe <akakabeseiji0@gmail.com>
2024-03-09 15:26:29 -0500

    remove pesky apostrophe

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,6 +2,6 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
   return <div className={styles.footer}>
-    <h1>Let's get in touch</h1>
+    <h1>Lets get in touch</h1>
   </div>
 }
