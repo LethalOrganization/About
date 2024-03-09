e2a844368a6b9f22b0a66589cf584608e87b9d76
Seiji Akakabe <akakabeseiji0@gmail.com>
2024-03-09 15:13:15 -0500

    add basic page blocks

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return <div className={styles.footer}>
+    <h1>Let's get in touch</h1>
+  </div>
+}
---
+++ app/components/services/Services.tsx
@@ -0,0 +1,5 @@
+export default function Services() {
+  return <div>
+    <h1>What we offer</h1>
+  </div>
+}
