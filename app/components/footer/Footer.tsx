441520df2527ce320621bf6a46bc7358c32acb03
Seiji Akakabe <akakabeseiji0@gmail.com>
2024-03-10 15:03:21 -0400

    tinker with page blocks

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,9 @@
+import PageBlock from '../pageBlock/PageBlock';
 import styles from './Footer.module.css';
 
 export default function Footer() {
-  return <div className={styles.footer}>
+  return <PageBlock className={styles.footer}>
     <h1>Lets get in touch</h1>
-  </div>
+    <p>IDK what to put here yet</p>
+  </PageBlock>
 }
