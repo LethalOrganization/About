a3f4944f03ad396ab296dd874141a7644e667394
Matthieu Constant <91146150+MattConstant@users.noreply.github.com>
2024-03-10 18:28:08 -0400

    added background

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './Hero.module.css';
+import { BackgroundBeams } from '../background/background-beams';
 
 export interface HeroProps {
   title: string,
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Hero from "./components/hero/Hero";
 import Footer from "./components/footer/Footer";
 import About from "./components/about/About";
 import Services from "./components/services/Services";
+import { BackgroundBeams } from "./components/background/background-beams";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+<main>
+      <div className="background-beams">
+        <BackgroundBeams />
+      </div>
       <Hero title="Lethal Org." subtitle="Consulting services" backgroundImageUrl="/images/lethal.gif" />
       <About imageSrc="/images/nf.jpg"/>
-      <Services/>
-      <Footer/>
+        <Services/>
+        <Footer/>
     </main>
   );
 }
